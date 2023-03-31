15b8bff9fcbc32e123f2fbe214078a6bf4a988a3
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-03-31 08:13:14 -0700

    Added api call to backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,39 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useState, useEffect } from "react";
 import MyNavBar from "./components/Navbar";
 import WeatherCard from "./components/WeatherCard";
 import WeatherForm from "./components/WeatherForm";
-
+import MyForm from "./components/Form.jsx";
+import DropDown from "./components/countrycodes";
+import ListStudents from './components/ListStudents'
 
 function App() {
   const [city, setCity] = useState("");
   const [result, setResult] = useState(null);
+  const [country, setCountry] = useState("");
 
   //A function to do the get request and set the state from openweather api
   const loadCity = (city) => {
     // pass city name as a param
     const params = new URLSearchParams({ cityName: city });
     // fetch the data from the backend
-    fetch(`http://localhost:8081/weather?${params}`)
+    fetch(`http://localhost:8080/weather?${params}`)
+      .then((response) => response.json())
+      .then((result) => {
+        // console.log("this is the data: ", result)
+        setCity(result.name);
+        setResult(result);
+      });
+  };
+
+  //A function to do the get request and set the state from openweather api
+  const loadCityCountry = (city, country) => {
+    let formatSearch = `${city},${country}`;
+    // pass city name as a param
+    const params = new URLSearchParams({ searchCity: formatSearch });
+    // fetch the data from the backend
+    fetch(`http://localhost:8080/weatherCountry?${params}`)
       .then((response) => response.json())
       .then((result) => {
         // console.log("this is the data: ", result)
@@ -35,16 +54,33 @@ function App() {
     e.preventDefault();
     // pass in the city to loadCity to get data from backend
     console.log(city);
-    loadCity(city);
+    if (country && city) {
+      loadCityCountry(city, country);
+    } else {
+      loadCity(city);
+    }
     // reset search bar to nothing
     setCity("");
+    setCountry("");
   };
 
   return (
     <div className="App">
       <MyNavBar />
-     <WeatherForm city={city} handleCityChange={handleCityChange} handleSubmit={handleSubmit}/>
-      {!result ? <p></p> : <WeatherCard data={result} /> }
+      <div className="AppOuter">
+        <div className="AppInner">
+          <WeatherForm
+            city={city}
+            handleCityChange={handleCityChange}
+            handleSubmit={handleSubmit}
+          />
+          <DropDown country={country} setCountry={setCountry} />
+           {!result ? <p></p> : <WeatherCard data={result} /> }
+        </div>
+        <div className="DropDown" >
+        </div>
+      </div>
+      <ListStudents />
     </div>
   );
 }
