007739e04af155ec1de265d2d0f99f5df29325a3
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-03-31 08:07:56 -0700

    api call to weatherapi

--- server/server.js
+++ server/server.js
@@ -93,10 +93,31 @@ app.put("/api/students/:studentId", async (req, res) => {
 
 // api call to weather app 
 app.get("/weather", (req, res) => {
-  const city = req.query.cityName;
-  const apiKey = process.env.API_KEY;
   const params = new URLSearchParams({
     q: req.query.cityName,
+    appid: process.env.API_KEY,
+    units: "imperial",
+  });
+  const url = `https://api.openweathermap.org/data/2.5/weather?${params}`;
+  
+  console.log(url);
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      res.send({ data });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+app.get("/weatherCountry", (req, res) => {
+  // console.log(req.query.searchCity);
+  // res.send(data)
+
+  const params = new URLSearchParams({
+    q: req.query.searchCity,
     appid: process.env.API_KEY,
     units: "imperial",
   });
@@ -117,3 +138,51 @@ app.get("/weather", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Hola, Server listening on ${PORT}`);
 });
+
+
+data = {
+    "data": {
+        "coord": {
+            "lon": -122.3321,
+            "lat": 47.6062
+        },
+        "weather": [
+            {
+                "id": 804,
+                "main": "Clouds",
+                "description": "overcast clouds",
+                "icon": "04d"
+            }
+        ],
+        "base": "stations",
+        "main": {
+            "temp": 45.52,
+            "feels_like": 38.64,
+            "temp_min": 42.67,
+            "temp_max": 49.23,
+            "pressure": 1010,
+            "humidity": 76
+        },
+        "visibility": 10000,
+        "wind": {
+            "speed": 15.99,
+            "deg": 193,
+            "gust": 18.99
+        },
+        "clouds": {
+            "all": 100
+        },
+        "dt": 1680195632,
+        "sys": {
+            "type": 2,
+            "id": 2041694,
+            "country": "US",
+            "sunrise": 1680184327,
+            "sunset": 1680230113
+        },
+        "timezone": -25200,
+        "id": 5809844,
+        "name": "Seattle",
+        "cod": 200
+    }
+}
