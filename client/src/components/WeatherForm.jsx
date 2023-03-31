b84bbe3b0e8a2d02eb09b4bfaf757a76c5f695c5
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-03-31 08:14:43 -0700

    added font awesome icons

--- client/src/components/WeatherForm.jsx
+++ client/src/components/WeatherForm.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/fontawesome-free-solid";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 const WeatherForm = (props) => {
   //what is inside props = {city, handleCityChange, handleSubmit}
   return (
