17cba9149efe38a7d4c0208c72a9d0e1a53560b7
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-03-31 08:04:47 -0700

    mock data

---
+++ server/data1.js
@@ -0,0 +1,46 @@
+export const data = {
+    "data": {
+        "coord": {
+            "lon": -122.3321,
+            "lat": 47.6062
+        },
+        "weather": [
+            {
+                "id": 804,
+                "main": "Clouds",
+                "description": "overcast clouds",
+                "icon": "04d"
+            }
+        ],
+        "base": "stations",
+        "main": {
+            "temp": 45.52,
+            "feels_like": 38.64,
+            "temp_min": 42.67,
+            "temp_max": 49.23,
+            "pressure": 1010,
+            "humidity": 76
+        },
+        "visibility": 10000,
+        "wind": {
+            "speed": 15.99,
+            "deg": 193,
+            "gust": 18.99
+        },
+        "clouds": {
+            "all": 100
+        },
+        "dt": 1680195632,
+        "sys": {
+            "type": 2,
+            "id": 2041694,
+            "country": "US",
+            "sunrise": 1680184327,
+            "sunset": 1680230113
+        },
+        "timezone": -25200,
+        "id": 5809844,
+        "name": "Seattle",
+        "cod": 200
+    }
+}
