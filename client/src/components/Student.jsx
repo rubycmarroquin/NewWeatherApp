7c10600658271d6561bdb51a2b0ed632e325b622
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-03-31 08:14:21 -0700

    added modal component for eeach city

--- client/src/components/Student.jsx
+++ client/src/components/Student.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import * as ioicons from 'react-icons/io5'
+import CityModal from './ShowCity';
 
 const Student = ({student, toUpdate, toDelete}) => {
 
     const onUpdate = (toUpdateStudent) => {
-        toUpdate(toUpdateStudent)
+        toUpdate(toUpdateStudent);
     }
 
     const onDelete = (toDeleteStudent) => {
@@ -19,6 +20,7 @@ const Student = ({student, toUpdate, toDelete}) => {
             <Card.Title>{student.firstname} {student.lastname}</Card.Title>
             <Button variant="outline-danger" onClick={()=>{onDelete(student)}} style={{padding: '0.6em', marginRight:'0.9em'}}><ioicons.IoTrash/></Button>
             <Button variant="outline-info" onClick={()=>{onUpdate(student)}} style={{padding: '0.6em'}}> <ioicons.IoSync/></Button>
+            <br/><CityModal student={student}/>
             </Card.Body>
         </Card>
     )
